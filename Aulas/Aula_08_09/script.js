bb9bcf0ed2f00cbe6206efe814cbcd40bb676983
Gabi <gabriellebetto@gmail.com>
2024-07-24 21:21:07 -0300

    Aulas

--- Aulas/Aula_08_09/script.js
+++ Aulas/Aula_08_09/script.js
@@ -1,2 +1,46 @@
 //alert("funcionou!")
-const input =window.document.getElementById("input");
+//local onde vai acessar window.document
+
+//carrega input da pagina
+const input = window.document.getElementById("input");
+//carrega input da pagina
+const ul = window.document.getElementById("ul");
+
+//var permanece até o final
+//let variavel literaria criar por menos tempo
+
+//função que adiciona novos itens a minha lista
+
+// isso adiciona no file no armazenamento local
+const base_dados = window.localStorage.getItem("dados");
+
+if (base_dados == null){
+    //ele espera 2 parametros nome dados e o valor
+    window.localStorage.setItem("dados", "[]");
+}
+var alunos =[];
+function carregar_dados (){
+    console.log(JSON.parse)
+}
+
+function adicionar(){
+    if (input.value !== ""){
+        const li = window.document.createElement("li");
+        //carrega o valor digitado no input na li
+        console.log(input.value);
+        
+        li.innnerHTML= input.value;
+        //aplica a "li" dentro da "ul"
+        ul.appendChild(li);
+        // console.log(input.value);
+
+        //limpa o input
+        input.value ="";
+    }else{
+        alert("O Campo está vazio!");
+    }
+}
+
+function adicionar(){
+
+}
