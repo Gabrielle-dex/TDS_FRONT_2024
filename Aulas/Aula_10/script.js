687a3454a904890bb748832eb229269d6ce023c1
Gabi <gabriellebetto@gmail.com>
2024-07-30 02:33:48 -0300

    Aulas

--- Aulas/Aula_10/script.js
+++ Aulas/Aula_10/script.js
@@ -1,4 +1,5 @@
 // ele precisa passar um valor ou pode ser um int
+// && E
 // posição = p
 // ultimo valor clicado = uvc
 const tabuleiro = document.getElementsByClassName("campo");
@@ -16,14 +17,32 @@ function validador (p){
        tabuleiro[p].innerHTML = "1";
        uvc ="O"
      }
+     valida_tabuleiro("x");
+     valida_tabuleiro("O");
     }
 
     //validadores
-    if(tabuleiro[0].innerHTML == tabuleiro[1].innerHTML && tabuleiro[0].innerHTML == tabuleiro[2].innerHTML){
-        //mostrar para o usuario
-        console.log("Parabéns!");
-    }else if{
-        (tabuleiro[3].innerHTML == tabuleiro[4].innerHTML && tabuleiro[3].innerHTML == tabuleiro[5].innerHTML)
-    
+// (tabuleiro[0].innerHTML == tabuleiro[1].innerHTML && tabuleiro[0].innerHTML == tabuleiro[2].innerHTML) ideia de formato
+function valida_tabuleiro(valor){
 
-}
+// linha 0, 1, 2 do tabuleiro se todos eles tiver o mesmo é valido
+    if(tabuleiro[0].innerHTML == valor && tabuleiro[1].innerHTML  == valor && tabuleiro[2].innerHTML == valor ){
+        //mostrar para o usuario
+        console.log("Fechou linha 1");
+    } else if (tabuleiro[3].innerHTML == valor && tabuleiro[4].innerHTML == valor && tabuleiro[5].innerHTML == valor);{
+        console.log("Fechou linha 2");
+    } else if (tabuleiro[6].innerHTML == valor && tabuleiro[7].innerHTML == valor && tabuleiro[8].innerHTML == valor){
+        console.log("Fecho linha 3");
+    } else if (tabuleiro[0].innerHTML == valor && tabuleiro[3].innerHTML == valor && tabuleiro[6].innerHTML == valor){
+        console.log("Fechou coluna 1");
+    } else if (tabuleiro[1].innerHTML == valor && tabuleiro[4].innerHTML == valor && tabuleiro[7].innerHTML == valor){
+        console.log("Fechou coluna 2");
+    } else if (tabuleiro[2].innerHTML == valor && tabuleiro[5].innerHTML == valor && tabuleiro[8].innerHTML == valor){
+        console.log("Fechou a coluna 3");
+    }else 
+        if
+        (tabuleiro[0].innerHTML == valor && tabuleiro[4].innerHTML == valor && tabuleiro[8].innerHTML == valor) {
+            console.log("Fechou diagonal 1");
+        } else if (tabuleiro[2].innerHTML == valor && tabuleiro[4].innerHTML == valor && tabuleiro[6].innerHTML == valor) {
+            console.log("Fechou diagonal 2");
+        }
