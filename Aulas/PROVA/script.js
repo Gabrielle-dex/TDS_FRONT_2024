e2ab77db998dc7fa0447f844d7ed54950dc54856
Gabi <gabriellebetto@gmail.com>
2024-08-05 20:46:23 -0300

    prova mais aula 11

---
+++ Aulas/PROVA/script.js
@@ -0,0 +1,16 @@
+//Ler valores nome e email | input
+const mensagem = document.getElementsById("mensagem_confirmacao");
+
+function mensagem_confirmacao (mensagem_confirmacao){
+
+ if (button.innerHTML == onclick){
+
+    console.log("Nome caputurado");
+    console.log("Email capturado");
+ 
+    mensagem.innerHTML = `Muito obrigado(a), senhor(a) : ${nome_capturado_input}
+    Acabamos de enviar um e-mail para: ${email_capturado_input}.`;
+    return true;
+ }
+
+}
