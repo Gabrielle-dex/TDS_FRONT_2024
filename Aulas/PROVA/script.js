20cafb1e82b7dba48b78521d823cf223d03f65bf
Gabi <gabriellebetto@gmail.com>
2024-08-12 22:13:34 -0300

    provas

--- Aulas/PROVA/script.js
+++ Aulas/PROVA/script.js
@@ -1,16 +1,20 @@
 //Ler valores nome e email | input
-const mensagem = document.getElementsById("mensagem_confirmacao");
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const mensagem = document.getElementsById("mensagemconfirmacao");
 
-function mensagem_confirmacao (mensagem_confirmacao){
+function mensagemconfirmacao (){
 
  if (button.innerHTML == onclick){
 
     console.log("Nome caputurado");
     console.log("Email capturado");
  
-    mensagem.innerHTML = `Muito obrigado(a), senhor(a) : ${nome_capturado_input}
-    Acabamos de enviar um e-mail para: ${email_capturado_input}.`;
+    mensagem.innerHTML = `Muito obrigado(a), senhor(a) : ${nome.value}
+    Acabamos de enviar um e-mail para: ${email.value}.`
     return true;
  }
 
 }
+
+// innerhtml é sobre um conteudo de uma tag
